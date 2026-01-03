2c537e815f1c54f117a74bcd1a87541e2dd17cc4
Ippili Yaswanth Kumar <131459565+NickyIm04020@users.noreply.github.com>
2026-01-03 14:58:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -129,14 +129,14 @@ const Portfolio = () => {
       id: 2,
       role: "Product Management Simulation",
       company: "Electronic Arts (EA) / Forage",
-      date: "Jun 2024 - Jun 2024",
+      period: "Jun 2024 - Jun 2024",
       description: "Improved retention forecast by 15% using feature prioritization; drafted Jira and stakeholder reports."
     },
     {
       id: 3,
       role: "Python Internship",
       company: "Codsoft",
-      date: "May 2024 - Jun 2024",
+      period: "May 2024 - Jun 2024",
       description: "Developed three interactive python applications—a secure password generator, dynamic calculator, and responsive to-do list—during a focused Web Development internship."
     },
   ];
